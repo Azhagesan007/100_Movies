9a437ebb81e5d1df820ba1adecee1a6dcd96e7ae
Azhagesan.Y <109057165+Azhagesan007@users.noreply.github.com>
2023-08-07 15:22:07 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+data = requests.get("https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/")
+
+soup = BeautifulSoup(data.text, "html.parser")
+print(soup.prettify())
+movie_name = soup.find_all(name="h3", class_="title")
+# print(soup)
+print(movie_name)
+movies = []
+for i in movie_name:
+    movies.append(i.get_text())
+movies.reverse()
+print(movies)
+for k in movies:
+    with open(file="movie.txt", mode="a", encoding="utf-8") as file:
+
+        file.write(f"{k}\n")
+        print(k)
